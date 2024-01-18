1923a55175cc83da65071f394dc430b574e55fa9
Frederick-js <faivergarzon@gmail.com>
2024-01-18 17:55:29 -0500

    terminando clase 6 de nunmeros

---
+++ 04-Numeros/js/05-app.js
@@ -0,0 +1,10 @@
+let puntaje = 5;
+
+puntaje++; // 5
+
+++puntaje; // 6
+
+
+puntaje -= 3;
+puntaje += 4;
+console.log(puntaje);
---
+++ 04-Numeros/js/06-app.js
@@ -0,0 +1,11 @@
+const numero1 = "20";
+const numero2 = "20.2";
+const numero3 = "Uno";
+const numero4 = 20;
+
+// convertir de string a un numero
+
+console.log(numero1);
+console.log(Number.parseInt(numero1));
+console.log(Number.parseFloat(numero4));
+
