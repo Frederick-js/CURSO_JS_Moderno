77c4fba13238b792a403c451fc833ca03fae9fdf
Frederick-js <faivergarzon@gmail.com>
2024-01-18 16:50:53 -0500

    numeros clase 4 temrinado

---
+++ 04-Numeros/js/04-app.js
@@ -0,0 +1,13 @@
+let resultado;
+
+resultado = 20 + 30 * 2;
+resultado = (20 + 30) * 2;
+
+
+// 20% de descuento en tu carrito de compras / sumamos y luego multiplicamos por .2 q seria el 20% de descuento
+resultado = (20 + 30 + 40 + 50 + 60) * .2;
+console.log(resultado);
+
+// agregarle el impuesto del 16%  se usa *1.16
+resultado = (20 + 30)* 1.16;
+console.log(resultado);
