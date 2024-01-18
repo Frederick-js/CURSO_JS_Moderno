c161b1a09c5e69772f00234d321f01b6fee92218
Frederick-js <faivergarzon@gmail.com>
2024-01-18 13:59:37 -0500

    numeros clase 3 temrinado

--- 04-Numeros/js/02-app.js
+++ 04-Numeros/js/02-app.js
@@ -1,1 +1,24 @@
-aasd
+const numero1 = 30;
+const numero2 = 20;
+
+let resultado;
+
+console.log(resultado);
+
+// SUMA
+resultado = numero1 + numero2;
+console.log(resultado);
+//RESTA
+resultado = numero1 - numero2;
+console.log(resultado);
+// MULTIPLICACION
+resultado = numero1 * numero2;
+console.log(resultado);
+//DIVISION
+resultado = numero1 / numero2;
+console.log(resultado);
+// MODULO    si divido 30 entre 20 cuando es el residuo. 
+resultado = numero1 % numero2;
+console.log(resultado);
+
+console.log(resultado);
---
+++ 04-Numeros/js/03-app.js
@@ -0,0 +1,53 @@
+let resultado;
+
+//PI
+resultado = Math.PI;
+console.log(resultado);
+
+
+// redondear
+resultado = Math.round(2.8);
+console.log(resultado);
+resultado = Math.round(2.2);
+console.log(resultado);
+resultado = Math.round(2.6);
+console.log(resultado);
+resultado = Math.round(2.5);
+
+// redondear hacia arriba
+resultado = Math.ceil(3.1);
+console.log(resultado);
+
+// redondear hacia arriba
+resultado = Math.floor(3.1);
+console.log(resultado);
+
+
+// raiz cuadrada
+resultado = Math.sqrt(144);
+console.log(resultado);
+
+
+// valor absoluto
+resultado = Math.abs(-500);  // trae el numero absoluto sin negativo
+console.log(resultado);
+
+// Potencia
+resultado = Math.pow(8,3); // 8 x 8 x 8
+console.log(resultado);
+
+// minimo
+resultado = Math.min(2,5,3,8,1); // nos trae el minimo
+console.log(resultado);
+
+// maximo
+resultado = Math.max(2,5,3,8,1); // nos trae el maximo
+console.log(resultado);
+
+// aleatorio
+resultado = Math.random();
+console.log(resultado);
+
+// si queremos redondearlo
+resultado = Math.floor(Math.random()*20);
+console.log(resultado);
