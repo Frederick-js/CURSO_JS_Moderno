786a080a5d82f26d6a02f89e508800fc8b7b213c
Frederick-js <faivergarzon@gmail.com>
2024-01-23 01:31:46 -0500

    a

--- 09-Funciones/js/09-app.js
+++ 09-Funciones/js/09-app.js
@@ -1,9 +1,19 @@
 // supongamos que vamos a crear un reproductor de musica
 
 const reproductor = {    // Esto serian un objeto
-    reproducir: function(id){ // reproducir seria una propiedad pero como es una funcion se conoce como metodo de propiedad
+    reproducir: function(id){ // reproducir: seria una propiedad de un objeto pero como es una funcion se conoce como metodo de propiedad
         console.log(`Reproduciendo cancion... ${id}`);
+    },
+    pausar: function(){
+        console.log('Pausando...');
     }
 }
 
-reproductor.reproducir(a);
+reproductor.reproducir(30);
+reproductor.pausar();
+
+reproductor.borrar = function(){
+    console.log('Borrando cancion...');
+}
+
+reproductor.borrar();
