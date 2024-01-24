1587ea50ba2cfe36c42bb71bed48650f5bb69947
Frederick-js <faivergarzon@gmail.com>
2024-01-24 09:38:48 -0500

    iniciando estructuras de control

--- 09-Funciones/js/13-app.js
+++ 09-Funciones/js/13-app.js
@@ -1,12 +1,23 @@
 // arrow functions metodos flecha en el reproductor de musica
 
 const reproductor = {
+  cancion : '',
   reproducir: id => console.log(`Reproduciendo cancion... ${id}`),
   pausar: () => console.log("Pausando..."),
   borrar: id => console.log(`borrando cancion... ${id}`),
   playList: nombre => console.log(`Creando la playList de... ${nombre}`),
-  reproducirPlaylist: nombre => console.log(`Reproduciondo playList ${nombre}`)
+  reproducirPlaylist: nombre => console.log(`Reproduciondo playList ${nombre}`),
+  set nuevaCancion(cancion) {
+    this.cancion = cancion;
+    console.log(`Añadiendo ${cancion}`);
+  },
+  get obtenerCancion(){
+    console.log(`${this.cancion}`);
+  }
 }
+
+reproductor.nuevaCancion = 'Enter Sadman';
+reproductor.obtenerCancion;
 
 reproductor.reproducir(30);
 reproductor.pausar();
