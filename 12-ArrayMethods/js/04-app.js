bc9d4f62c38570afede092e260d6c1f457cdbc51
Frederick-js <faivergarzon@gmail.com>
2024-01-28 19:35:14 -0500

    metodos array

--- 12-ArrayMethods/js/04-app.js
+++ 12-ArrayMethods/js/04-app.js
@@ -1,6 +1,6 @@
 const carrito = [ // y esto es un arreglo de objetos
     { nombre: 'Monitor 27 Pulgadas', precio: 500 },
-    { nombre: 'Televisión', precio: 100 },
+    { nombre: 'Television', precio: 100 },
     { nombre: 'Tablet', precio: 200 },
     { nombre: 'Audifonos', precio: 300 },
     { nombre: 'Teclado', precio: 400 },
@@ -9,7 +9,26 @@ const carrito = [ // y esto es un arreglo de objetos
 
 
 let resultado ;
+let resultado2 ;
+let resultado3 ;
+let resultado4 ;
 
 resultado = carrito.filter(producto=> producto.precio > 400 );
 
+resultado2 = carrito.filter(producto => producto.precio < 600);
+
 console.log(resultado);
+
+console.log(resultado2);
+
+// ejemplo trae todos los objetos menos tablet
+
+resultado3 = carrito.filter(producto=> producto.nombre !== 'Tablet');
+
+console.log(resultado3);
+
+resultado4 = carrito.filter((producto)=>{
+    return producto.nombre == 'Television';
+})
+
+console.log(resultado4);
