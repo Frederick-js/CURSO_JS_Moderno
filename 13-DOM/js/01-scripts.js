a6934c41a8f7df4cd8389bc51b996f19394b92f0
Frederick-js <faivergarzon@gmail.com>
2024-01-30 00:01:47 -0500

    dom

---
+++ 13-DOM/js/01-scripts.js
@@ -0,0 +1,20 @@
+let elemento;
+elemento = document;
+console.log(elemento);
+elemento = document.all;
+console.log(elemento);
+elemento = document.head;
+console.log(elemento);
+elemento = document.body;
+console.log(elemento);
+elemento = document.domain;
+
+console.log(elemento);
+elemento = document.forms;
+
+
+
+
+
+
+console.log(elemento);
