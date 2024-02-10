342699d6e8f7e4c0afc397744b4ce74560f8c1de
p4G8eUL3 <faivergarzon@gmail.com>
2024-02-09 20:45:51 -0500

    agregando elementos y vaciando html

--- 15 - proyectos/15-PROYECTO-Carrito/js/app.js
+++ 15 - proyectos/15-PROYECTO-Carrito/js/app.js
@@ -2,6 +2,10 @@ const carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos')
+let articulosCarrito = [];
+
+
+
 
 cargarEventListeners();
 function cargarEventListeners() {
@@ -14,6 +18,65 @@ function cargarEventListeners() {
 function agregarCurso(e) {
     e.preventDefault();
     if (e.target.classList.contains('agregar-carrito')) {
-        console.log('Agregando al carrito');
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
     }
 }
+
+
+// lee el contenido del html y extrae informacion
+function leerDatosCurso(curso) {
+
+    // crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+    
+    // agrega elementos al arreglo de carrito
+    articulosCarrito = [...articulosCarrito, infoCurso];
+    carritoHtml();
+}
+
+
+
+
+// MUestra el carrito de compras en el html
+function carritoHtml(){
+
+    //Limpiar el html
+    limpiarHtml(); // con esta funciona limpiamos el html colocandolo vacio
+
+// recorre el carrito y genera el html
+    articulosCarrito.forEach(curso => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>
+                ${curso.titulo}
+            </td>
+        `;
+
+        // agrega el html del carrito en el tbody
+        contenedorCarrito.appendChild(row); // appendChild Agrega un nuevo nodo al final de la lista de un elemento hijo de un elemento padre especificado.
+    })
+}
+
+
+//Eliminar los curcos del tbody
+function limpiarHtml() {
+    
+    // forma lenta
+    // contenedorCarrito.innerHTML = '';
+
+    while (contenedorCarrito.firstChild) {
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+    }
+}
+
+vaciarCarritoBtn.addEventListener('click',()=>{
+    limpiarHtml();
+})
+
