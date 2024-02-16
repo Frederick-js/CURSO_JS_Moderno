af1e4f0f9a5d3a8c5cf908e5f0798c2e363abf92
p4G8eUL3 <faivergarzon@gmail.com>
2024-02-16 01:47:37 -0500

    ocultar alertas si pasa validacion

--- 15 - proyectos/16-PROYECTO-EnviarEmail/js/app.js
+++ 15 - proyectos/16-PROYECTO-EnviarEmail/js/app.js
@@ -1,44 +1,44 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener("DOMContentLoaded", function () {
+  //Seleccionar los elementos de la interfaz
+  const inputEmail = document.querySelector("#email");
+  const inputAsunto = document.querySelector("#asunto");
+  const inputMensaje = document.querySelector("#mensaje");
+  const formulario = document.querySelector("#formulario");
 
-    //Seleccionar los elementos de la interfaz
-    const inputEmail = document.querySelector('#email');
-    const inputAsunto = document.querySelector('#asunto');
-    const inputMensaje = document.querySelector('#mensaje');
-    const formulario = document.querySelector('#formulario');
+  // Asignar eventos
+  inputEmail.addEventListener("blur", validar); // colocar validar sin() es llamar una funcion hasta que suceda el evento. colocando si o si llama la funcion
+  inputAsunto.addEventListener("blur", validar);
+  inputMensaje.addEventListener("blur", validar);
 
-   // Asignar eventos
-   inputEmail.addEventListener('blur', validar);  // colocar validar sin() es llamar una funcion hasta que suceda el evento. colocando si o si llama la funcion
-   inputAsunto.addEventListener('blur', validar);
-   inputMensaje.addEventListener('blur', validar);
-
-    function validar(e){
-        
-        if (e.target.value.trim() === '') {
-            mostrarAlerta(`El Campo ${e.target.id} es obligatorio`,e.target.parentElement);
-        }else{
-            console.log('Si hay algo');
-        }
+  function validar(e) {
+    if (e.target.value.trim() === "") {
+      mostrarAlerta(
+        `El Campo ${e.target.id} es obligatorio`,
+        e.target.parentElement
+      );
+      return;
     }
 
+    limpiarAlerta(e.target.parentElement);
+  }
 
-    function mostrarAlerta(mensaje, referencia) {
+  function mostrarAlerta(mensaje, referencia) {
+    limpiarAlerta(referencia);
+    // Generar alerta en HTML
+    const error = document.createElement("P");
+    error.textContent = mensaje; // se agrega color de fondo
+    error.classList.add("bg-red-600", "text-white", "p-2", "text-center");
 
-        // Comprueba si ya existe una alerta
-        const alerta = referencia.querySelector('.bg-red-600');
+    // Inyectar al error del formulario
+    referencia.appendChild(error);
+  }
 
-        if (alerta) {
-            alerta.remove();
-        }
-            
-        // Generar alerta en HTML
-        const error = document.createElement('P');
-        error.textContent = mensaje; // se agrega color de fondo
-        error.classList.add('bg-red-600','text-white','p-2','text-center');
-
-        // Inyectar al error del formulario
-        referencia.appendChild(error);
-    
+  // funcion limpiar alerta
+  function limpiarAlerta(referencia) {
+    // Comprueba si ya existe una alerta
+    const alerta = referencia.querySelector(".bg-red-600");
+    if (alerta) {
+      alerta.remove();
     }
-
-
+  }
 });
