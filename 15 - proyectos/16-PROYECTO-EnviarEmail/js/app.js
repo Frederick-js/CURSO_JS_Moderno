f635dcd75d892edef6a18bb550a0e8fb78b85688
p4G8eUL3 <faivergarzon@gmail.com>
2024-02-15 20:44:32 -0500

    iniciando validar cada campo

--- 15 - proyectos/16-PROYECTO-EnviarEmail/js/app.js
+++ 15 - proyectos/16-PROYECTO-EnviarEmail/js/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
 
    // Asignar eventos
    inputEmail.addEventListener('blur', validar);  // colocar validar sin() es llamar una funcion hasta que suceda el evento. colocando si o si llama la funcion
@@ -22,9 +23,10 @@ document.addEventListener('DOMContentLoaded', function(){
     function mostrarAlerta() {
         // Generar alerta en HTML
         const error = document.createElement('P');
-        error.textContent = 'Hubo un error...';
-
-        console.log(error);
+        error.textContent = 'Hubo un error...'; // se agrega color de fondo
+        error.classList.add('bg-red-600','text-white','p-2','text-center');
+        // Inyectar al error del formulario
+        formulario.appendChild(error);
     }
 
 
