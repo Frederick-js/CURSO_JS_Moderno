0915997f45eeb92e143aec4d64ab64310f4fdda4
p4G8eUL3 <faivergarzon@gmail.com>
2024-02-17 21:16:58 -0500

    on off boton enviar

--- 15 - proyectos/16-PROYECTO-EnviarEmail/js/app.js
+++ 15 - proyectos/16-PROYECTO-EnviarEmail/js/app.js
@@ -1,11 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+  const email= {
+    email: '',
+    asunto: '',
+    autor: '',
+    mensaje: ''
+  };
+
+  console.log(email);
   //Seleccionar los elementos de la interfaz
   const inputEmail = document.querySelector("#email");
   const inputAsunto = document.querySelector("#asunto");
   const inputAutor = document.querySelector("#autor");
   const inputMensaje = document.querySelector("#mensaje");
   const formulario = document.querySelector("#formulario");
+  const btnSubmit = document.querySelector("#formulario button[type='submit']");
 
+  console.log(btnSubmit);
   // Asignar eventos
   inputEmail.addEventListener("blur", validar); // colocar validar sin() es llamar una funcion hasta que suceda el evento. colocando si o si llama la funcion
   inputAsunto.addEventListener("blur", validar);
@@ -18,16 +29,27 @@ document.addEventListener("DOMContentLoaded", function () {
         `El Campo ${e.target.id} es obligatorio`,
         e.target.parentElement
       );
+      email[e.target.name] = ""; // reiniciamos
+      comprobarEmail(); // comprobamos el email
       return;
     }
    
     if (e.target.id === 'email' && !validarEmail(e.target.value)) {
       mostrarAlerta('El email no es valido', e.target.parentElement);
+      email[e.target.name] = "";// reiniciamos
+      comprobarEmail();// comprobamos el email
       return;
     }
     
 
     limpiarAlerta(e.target.parentElement);
+
+    //Asignar los valores
+    email[e.target.name] = e.target.value.trim().toLowerCase();
+    
+    // comprobar el objeto del email
+    comprobarEmail();
+
   }
 
   function mostrarAlerta(mensaje, referencia) {
@@ -56,4 +78,13 @@ document.addEventListener("DOMContentLoaded", function () {
     return resultado;
   };
 
+    function comprobarEmail() {
+      if (Object.values(email).includes('')) {
+        btnSubmit.classList.add('opacity-50');
+          btnSubmit.disabled = true;
+      }else{
+          btnSubmit.classList.remove('opacity-50');
+          btnSubmit.disabled = false;
+      }
+    }
 });
