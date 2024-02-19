4429758a8283970bfdd5408cc91a57cfb7551d4c
p4G8eUL3 <faivergarzon@gmail.com>
2024-02-19 11:05:22 -0500

    leer el valor de la marca del automovil

--- 15 - proyectos/17-PROYECTO-Buscador/js/app.js
+++ 15 - proyectos/17-PROYECTO-Buscador/js/app.js
@@ -1,10 +1,31 @@
 // variables
-const resultado = document.querySelector('#resultado');
+const marca = document.querySelector('#marca');
 const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+
+// contenedor para los resultados
+const resultado = document.querySelector('#resultado');
+
 
 const max = new Date().getFullYear();
 const min = max - 10
 
+// Generar un objeto con la busqueda
+const datosBusqueda = {
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color: ''
+
+}
 
 //EVENTOS
 
@@ -17,7 +38,41 @@ document.addEventListener('DOMContentLoaded', ()=>{
 })
 
 
+// event listener para los select de busqueda
+marca.addEventListener('change', e=> {
+    datosBusqueda.marca = e.target.value;
+    console.log(datosBusqueda);
+})
 
+year.addEventListener('change', e=> {
+    datosBusqueda.year = e.target.value;
+    console.log(datosBusqueda);
+})
+
+minimo.addEventListener('change', e=> {
+    datosBusqueda.minimo = e.target.value;
+    console.log(datosBusqueda);
+})
+
+maximo.addEventListener('change', e=> {
+    datosBusqueda.maximo = e.target.value;
+    console.log(datosBusqueda);
+})
+
+puertas.addEventListener('change', e=> {
+    datosBusqueda.puertas = e.target.value;
+    console.log(datosBusqueda);
+})
+
+transmision.addEventListener('change', e=> {
+    datosBusqueda.transmision = e.target.value;
+    console.log(datosBusqueda);
+})
+
+color.addEventListener('change', e=> {
+    datosBusqueda.color = e.target.value;
+    console.log(datosBusqueda);
+})
 
 
 // Funciones
