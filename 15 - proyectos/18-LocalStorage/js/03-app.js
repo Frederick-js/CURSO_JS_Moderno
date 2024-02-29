2f7cc5e826cf0a982bee401240b5d2345da882dd
p4G8eUL3 <faivergarzon@gmail.com>
2024-02-29 14:56:08 -0500

    localstorage eliminar clear

---
+++ 15 - proyectos/18-LocalStorage/js/03-app.js
@@ -0,0 +1,11 @@
+// eliminar de localstorage
+localStorage.removeItem('Nombre');
+
+// Actualizar un registro
+const mesesArray = JSON.parse(localStorage.getItem('meses')); // convierte meses que esta en string ahora a objeto de array.
+console.log(mesesArray);
+mesesArray.push('Nuevo Mes');
+console.log(mesesArray);
+localStorage.setItem('meses', JSON.stringify(mesesArray));
+
+localStorage.clear();
