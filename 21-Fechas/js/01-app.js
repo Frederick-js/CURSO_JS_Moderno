7a2ad821992cbf1cb8905cb23f361200c735c611
p4G8eUL3 <faivergarzon@gmail.com>
2024-03-10 14:35:11 -0500

    momentsjs fechas javascript

--- 21-Fechas/js/01-app.js
+++ 21-Fechas/js/01-app.js
@@ -5,5 +5,8 @@ let valor;
 valor = diaHoy;
 
 valor = diaHoy.getFullYear(); // trae el year 1996
+valor = diaHoy.getMonth(); // enero como el mes 0 y febrero como el mes 1
+valor = diaHoy.getMinutes();
+valor = diaHoy.getHours();
 
 console.log(valor);
---
+++ 21-Fechas/js/02-app.js
@@ -0,0 +1,5 @@
+const diaHoy = new Date();
+// momentjs.com
+moment.locale("es");
+console.log(moment().format("MMMM Do YYYY h:m"));
+console.log(moment().format("LLLL"));
