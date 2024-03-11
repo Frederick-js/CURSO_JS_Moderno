b5a309e639cedc079494ab174afd106f592b89ac
p4G8eUL3 <faivergarzon@gmail.com>
2024-03-11 17:31:43 -0500

    heredar un prototype

--- 22-Prototypes/js/04-app-DESKTOP-M77847L.js
+++ 22-Prototypes/js/04-app-DESKTOP-M77847L.js
@@ -26,23 +26,3 @@ Cliente.prototype.nombreClienteSaldo = function () {
 Cliente.prototype.retiraSaldo = function (retira) {
   this.saldo -= retira;
 };
-
-function Persona(nombre, saldo, telefono) {
-  Cliente.call(this, nombre, saldo);
-  this.telefono = telefono;
-}
-
-Persona.prototype = Object.create(Cliente.prototype);
-
-Persona.prototype.constructor = Cliente;
-
-// instanciarlo
-const juan = new Persona("Juan", 5000, 32212443);
-console.log(juan);
-console.log(juan.nombreClienteSaldo());
-
-Persona.prototype.mostrarTelefono = function () {
-  return `El telefono de esta persona es ${this.telefono}`;
-};
-
-console.log(juan.mostrarTelefono());
