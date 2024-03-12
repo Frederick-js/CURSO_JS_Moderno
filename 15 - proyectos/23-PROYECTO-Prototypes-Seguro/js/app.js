be8da0264bb6773653178a34d3fe05d7ff24ed9b
p4G8eUL3 <faivergarzon@gmail.com>
2024-03-11 22:25:54 -0500

    creando options year

--- 15 - proyectos/23-PROYECTO-Prototypes-Seguro/js/app.js
+++ 15 - proyectos/23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -1,7 +1,29 @@
 // constructores
 
-function Seguro(marca, yerar, tipo) {
+function Seguro(marca, year, tipo) {
   this.marca = marca;
   this.year = year;
   this.tipo = tipo;
 }
+
+function UI() {}
+
+// Llena las opciones de los anos
+UI.prototype.llenarOpciones = () => {
+  const max = new Date().getFullYear(),
+    min = max - 24;
+  const selectYear = document.querySelector("#year");
+
+  for (let i = max; i > min; i--) {
+    let option = document.createElement("option");
+    option.value = i;
+    option.textContent = i;
+    selectYear.appendChild(option);
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("hola");
+});
+
+UI.prototype.llenarOpciones();
