8ded7bdef57e62cef7e7a84d7c1886f86ce9fa85
p4G8eUL3 <faivergarzon@gmail.com>
2024-03-22 00:21:27 -0500

    agregando un listener al formulario

--- 15 - proyectos/26-PROYECTO-AdministrarCitas/js/app.js
+++ 15 - proyectos/26-PROYECTO-AdministrarCitas/js/app.js
@@ -5,12 +5,16 @@ const emailInput = document.querySelector("#email");
 const fechaInput = document.querySelector("#fecha");
 const sintomasInput = document.querySelector("#sintomas");
 
+const formulario = document.querySelector("#formulario-cita");
+
 // Eventos
 pacienteInput.addEventListener("change", datosCita);
 propietaInput.addEventListener("change", datosCita);
 emailInput.addEventListener("change", datosCita);
 fechaInput.addEventListener("change", datosCita);
 sintomasInput.addEventListener("change", datosCita);
+
+formulario.addEventListener("submit", submitCita);
 
 // objeto de cita
 const citaObj = {
@@ -24,4 +28,9 @@ const citaObj = {
 function datosCita(e) {
   citaObj[e.target.name] = e.target.value;
   console.log(citaObj);
+}
+
+function submitCita(e) {
+  e.preventDefault();
+  console.log("submit al formulario");
 }
